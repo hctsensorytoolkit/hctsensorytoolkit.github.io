003e32a1c62f5709fd60bb90a4324dae695fa345
Grant Butler <butleragrant@gmail.com>
2017-02-28 10:50:12 -0800

    Added relationship descriptions and the lists for each individual type description. Next working on the suggested activities

--- script/script.js
+++ script/script.js
@@ -51,7 +51,46 @@ function setup() {
 	};
 
 	function update() {
+	    var allOutputs = document.getElementsByClassName("output");
+
+	    for(i = 0; i < allOutputs.length; i++) {
+		allOutputs[i].style.display = "none";
+	    }
 	    
+	    var element;
+	    if(childIndex == 0) {
+		switch(parentIndex) {
+		case 0:
+		    element = document.getElementById("output-over-over");
+		    break;
+		case 1:
+		    element = document.getElementById("output-under-over");
+		    break;
+		case 2:
+		    element = document.getElementById("output-neutral-over");
+		    break;
+		default:
+		    element = document.getElementById("output-unselected");
+		}
+	    } else if(childIndex == 1) {
+		switch(parentIndex) {
+		case 0:
+		    element = document.getElementById("output-over-under");
+		    break;
+		case 1:
+		    element = document.getElementById("output-under-under");
+		    break;
+		case 2:
+		    element = document.getElementById("output-neutral-under");
+		    break;
+		default:
+		    element = document.getElementById("output-unselected");
+		}
+	    } else {
+		element = document.getElementById("output-unselected");
+	    }
+
+	    element.style.display = "inline";
 	    
 	}
 
@@ -65,6 +104,12 @@ function setup() {
 
     for(i = 0; i < inputsChild.length; i++) {
 	inputsChild[i].addEventListener("click", outputModule.updateChild);
-    }  
+    }
+    
+    
+}
+
+function getOutputDesc(parentIndex, childIndex) {
+    
     
 }
